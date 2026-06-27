65218ebcb5e4029f94e4fef284c7d8d3fdaf7a89
Elizaveta Tomaeva <elizaveta_tomaeva@epam.com>
2026-06-28 01:16:53 +0300

    post-deploy fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build'
     },
   },
 }; 
